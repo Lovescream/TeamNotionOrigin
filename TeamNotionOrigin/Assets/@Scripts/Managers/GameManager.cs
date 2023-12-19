9ff197ae36d7207e8c81e5b75840349a52561156
정채운 <toyboy01113@gmail.com>
2023-12-19 14:27:05 +0900

    feat : UI리워드 기본 세팅

--- TeamNotionOrigin/Assets/@Scripts/Managers/GameManager.cs
+++ TeamNotionOrigin/Assets/@Scripts/Managers/GameManager.cs
@@ -4,26 +4,26 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class GameManager
 {
-    //private static Data.Player _player;
-    //public Data.Player Player { get { return _player; } }
-    //public void Initialize()
-    //{
-    //    SetGameData();
-    //}
+    private static Data.Player _player;
+    public Data.Player Player { get { return _player; } }
+    public void Initialize()
+    {
+        SetGameData();
+    }
 
-    //private void SetGameData()
-    //{
-    //    if (Main.Data.PlayerDict.TryGetValue(1, out Data.Player player))
-    //    {
-    //        _player = player;
-    //    }
+    private void SetGameData()
+    {
+        if (Main.Data.PlayerDict.TryGetValue(1, out Data.Player player))
+        {
+            _player = player;
+        }
 
-    //    Debug.Log(player.defence);
+        Debug.Log(player.defence);
 
-    //    if (Main.Data.ItemDict[Data.ItemType.Passive].TryGetValue(1, out Data.Item item1) && item1 is Data.Passive passiveItem)
-    //    {
-    //        Debug.Log(passiveItem.name);
-    //    }
+        if (Main.Data.ItemDict[Data.ItemType.Passive].TryGetValue(1, out Data.Item item1) && item1 is Data.Passive passiveItem)
+        {
+            Debug.Log(passiveItem.name);
+        }
 
-    //}
+    }
 }
