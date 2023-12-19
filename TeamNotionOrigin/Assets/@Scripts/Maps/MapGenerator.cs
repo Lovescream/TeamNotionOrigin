874bbf5e24f7dabcba698a8e75a40d100de5632b
wkdtjdrb_35 <81483103+zjarhdtjdrb@users.noreply.github.com>
2023-12-19 15:48:19 +0900

    fix: 코드 수정, 오브젝트 설정
    오브젝트 배치 겹치지 않게 수정, 벽 충돌 설정

--- TeamNotionOrigin/Assets/@Scripts/Maps/MapGenerator.cs
+++ TeamNotionOrigin/Assets/@Scripts/Maps/MapGenerator.cs
@@ -7,6 +7,7 @@ using UnityEngine.Tilemaps;
 
 public class MapGenerator : MonoBehaviour
 {
+    #region field
     [SerializeField] Vector2Int mapSize; //원하는 맵의 크기
     [SerializeField] float minimumDevideRate; //공간이 나눠지는 최소 비율
     [SerializeField] float maximumDivideRate; //공간이 나눠지는 최대 비율
@@ -16,9 +17,12 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] Tile wallTile;
     [SerializeField] Tile outTile;
     [SerializeField] private SpriteRenderer portalSprite;
+    [SerializeField] private SpriteRenderer mainSprite;
     [SerializeField] private SpriteRenderer monsterSprite_1;
     [SerializeField] private SpriteRenderer monsterSprite_2;
-    private RectInt biggestNode;
+    private RectInt biggestNode = new RectInt(int.MinValue, int.MinValue, int. MinValue, int.MinValue);
+    private RectInt smallestNode = new RectInt(100,100,100,100);
+    #endregion
 
     private void Start()
     {
@@ -29,8 +33,10 @@ public class MapGenerator : MonoBehaviour
         GenerateLoad(root, 0);
         FillWall();
         PlacePortalSpriteInRoom(biggestNode);
+        PlaceMainSpriteInRoom(smallestNode);
     }
 
+    #region Divide Node
     private void Divide(Node tree, int n)
     {
         if (n == maximumDepth) // 원하는 깊이에 도착하면, 더이상 나누지 않는다.
@@ -55,12 +61,14 @@ public class MapGenerator : MonoBehaviour
         Divide(tree.leftNode, n + 1);
         Divide(tree.rightNode, n + 1);
     }
+    #endregion
 
+    #region Generate Room
     private RectInt GenerateRoom(Node tree, int n)
     {
         RectInt rect;
         if (n == maximumDepth)
-        {    
+        {
             rect = tree.nodeRect;
 
             int width = Random.Range(rect.width / 2, rect.width - 1);
@@ -73,6 +81,7 @@ public class MapGenerator : MonoBehaviour
             FillRoom(rect);
             PlaceMonsterInRoom(rect);
             FindbiggestNodeRoom(rect);
+            FindsmallestNodeRoom(rect);
         }
         else
         {
@@ -82,23 +91,9 @@ public class MapGenerator : MonoBehaviour
         }
         return rect;
     }
-    private RectInt FindbiggestNodeRoom(RectInt rectInt)
-    {
-        if (rectInt.width * rectInt.height > biggestNode.width * biggestNode.height)
-        {
-            biggestNode = rectInt;
-        }
-        return biggestNode;
-    }
+    #endregion
 
-    private void PlacePortalSpriteInRoom(RectInt roomRect)
-    {
-        int x = roomRect.x -1+ roomRect.width ;
-        int y = roomRect.y -1+ roomRect.height ;
-
-        Instantiate(portalSprite, new Vector3(x - mapSize.x / 2, y - mapSize.y / 2, 0), Quaternion.identity);
-    }
-
+    #region Generate Load
     private void GenerateLoad(Node tree, int n)
     {
         if (n == maximumDepth)
@@ -106,21 +101,31 @@ public class MapGenerator : MonoBehaviour
         Vector2Int leftNodeCenter = tree.leftNode.center;
         Vector2Int rightNodeCenter = tree.rightNode.center;
 
+        //가로 load
         for (int i = Mathf.Min(leftNodeCenter.x, rightNodeCenter.x); i <= Mathf.Max(leftNodeCenter.x, rightNodeCenter.x); i++)
         {
-            tileMap.SetTile(new Vector3Int(i - mapSize.x / 2, leftNodeCenter.y - mapSize.y / 2, 0), roomTile);
+            for (int k = 0; k < 3; k++)
+            {
+                tileMap.SetTile(new Vector3Int(i - mapSize.x / 2, leftNodeCenter.y + k - mapSize.y / 2, 0), roomTile);
+            }
         }
 
+        // 세로 load
         for (int j = Mathf.Min(leftNodeCenter.y, rightNodeCenter.y); j <= Mathf.Max(leftNodeCenter.y, rightNodeCenter.y); j++)
         {
-            tileMap.SetTile(new Vector3Int(rightNodeCenter.x - mapSize.x / 2, j - mapSize.y / 2, 0), roomTile);
+            for (int k = 0; k < 3; k++)
+            {
+                tileMap.SetTile(new Vector3Int(rightNodeCenter.x + k - mapSize.x / 2, j - mapSize.y / 2, 0), roomTile);
+            }
         }
         //이전에 선으로 만들었던 부분을 room tile로 채우는 과정
 
         GenerateLoad(tree.leftNode, n + 1); //자식 노드 탐색
         GenerateLoad(tree.rightNode, n + 1);
     }
+    #endregion
 
+    #region Fill Tile
     void FillBackground()
     {
         for (int i = -10; i <= mapSize.x + 10; i++) // 맵 크기보다 넓게
@@ -167,23 +172,65 @@ public class MapGenerator : MonoBehaviour
             }
         }
     }
-   
+    #endregion
+
 
     private void PlaceMonsterInRoom(RectInt roomRect)
     {
-        int x1 = Random.Range(roomRect.x + 3, roomRect.x + roomRect.width - 3);
-        int y1 = Random.Range(roomRect.y + 3, roomRect.y + roomRect.height - 3);
+        float radius1 = Mathf.Max(monsterSprite_1.size.x, monsterSprite_1.size.y) / 2;
+        float radius2 = Mathf.Max(monsterSprite_2.size.x, monsterSprite_2.size.y) / 2;
+        float minDistance = radius1 + radius2; // 두 스프라이트의 최소 거리 설정
 
-        int x2 = Random.Range(roomRect.x + 3, roomRect.x + roomRect.width - 3);
-        int y2 = Random.Range(roomRect.y + 3, roomRect.y + roomRect.height - 3);
+        Vector2 pos1, pos2;
 
-        if (x1 == x2 && y1 == y2)
+        do
         {
-            PlaceMonsterInRoom(roomRect); // 두 몬스터가 동일한 위치에 생성되는 경우, 위치를 다시 생성
-            return;
-        }
+            int x1 = Random.Range(roomRect.x + 3, roomRect.x + roomRect.width - 3);
+            int y1 = Random.Range(roomRect.y + 3, roomRect.y + roomRect.height - 3);
 
-        Instantiate(monsterSprite_1, new Vector3(x1 - mapSize.x / 2, y1 - mapSize.y / 2, 0), Quaternion.identity);
-        Instantiate(monsterSprite_2, new Vector3(x2 - mapSize.x / 2, y2 - mapSize.y / 2, 0), Quaternion.identity);
+            int x2 = Random.Range(roomRect.x + 3, roomRect.x + roomRect.width - 3);
+            int y2 = Random.Range(roomRect.y + 3, roomRect.y + roomRect.height - 3);
+
+            pos1 = new Vector2(x1, y1);
+            pos2 = new Vector2(x2, y2);
+        }
+        while (Vector2.Distance(pos1, pos2) < minDistance); // 두 몬스터의 위치가 너무 가까우면 다시 생성
+
+        Instantiate(monsterSprite_1, new Vector3(pos1.x - mapSize.x / 2, pos1.y - mapSize.y / 2, 0), Quaternion.identity);
+        Instantiate(monsterSprite_2, new Vector3(pos2.x - mapSize.x / 2, pos2.y - mapSize.y / 2, 0), Quaternion.identity);
+    }
+
+    private void PlaceMainSpriteInRoom(RectInt roomRect)
+    {
+        int x = roomRect.x;
+        int y = roomRect.y;
+
+        Instantiate(mainSprite, new Vector3(x - mapSize.x / 2, y - mapSize.y / 2, 0), Quaternion.identity);
+    }
+
+    private void PlacePortalSpriteInRoom(RectInt roomRect)
+    {
+        int x = (roomRect.x - 1) + roomRect.width;
+        int y = (roomRect.y - 1) + roomRect.height;
+
+        Instantiate(portalSprite, new Vector3(x - mapSize.x / 2, y - mapSize.y / 2, 0), Quaternion.identity);
+    }
+
+    private RectInt FindsmallestNodeRoom(RectInt rectInt)
+    {
+        if (rectInt.width * rectInt.height < smallestNode.width * smallestNode.height)
+        {
+            smallestNode = rectInt;
+        }
+        return smallestNode;
+    }
+
+    private RectInt FindbiggestNodeRoom(RectInt rectInt)
+    {
+        if (rectInt.width * rectInt.height > biggestNode.width * biggestNode.height)
+        {
+            biggestNode = rectInt;
+        }
+        return biggestNode;
     }
 }
