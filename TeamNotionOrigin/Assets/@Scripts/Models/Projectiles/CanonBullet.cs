5d820ed8000bccb3a4a7a1b6d6feb7fcc8a283e2
GAMGDOG <chltjdwo98@gmail.com>
2023-12-21 03:59:23 +0900

    Update CanonBullet.cs

--- TeamNotionOrigin/Assets/@Scripts/Models/Projectiles/CanonBullet.cs
+++ TeamNotionOrigin/Assets/@Scripts/Models/Projectiles/CanonBullet.cs
@@ -9,26 +9,8 @@ public class CanonBullet : Projectile
 
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
-        transform.position += mousePoint.normalized;
-    }
-    protected override void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.TryGetComponent<Creature>(out var creature))
-        {
-            creature.Hp -= Damage;
-            Main.Object.Spawn<Boom>(1, transform.position);
-            Main.Object.Despawn(this);
-        }
-        if (collision.gameObject.layer == 0)
-        {
-            Main.Object.Spawn<Boom>(1, transform.position);
-            Main.Object.Despawn(this);
-        }
-
-    }
-}
         base.OnTriggerEnter2D(collision);
-        //Main.Object.Spawn<Boom>(1, transform.position); 폭발이펙트 생성 및 해당 이펙트와 닿는 적 데미지 판정
+        Main.Object.Spawn<Boom>(1, transform.position);
         Main.Object.Despawn(this);
     }
 }
